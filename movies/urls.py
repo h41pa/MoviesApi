d5b0c4d196ffeda604cc0832f37740be1de0f57a
h41pa <madalin_chelu@yahoo.com>
2024-04-03 11:28:59 +0300

    first commit

---
+++ movies/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import movie_list, movie_detail
+
+
+urlpatterns = [
+    path('', movie_list, name='movie_list'),
+    path('movies/<int:movie_id>/', movie_detail, name='movie_detail'),
+]
---
+++ movies/views.py
@@ -0,0 +1,57 @@
+import requests
+
+from django.shortcuts import render, get_object_or_404
+
+def movie_list(request):
+    search_query = request.GET.get('query')
+    page_number = request.GET.get('page', 1)
+
+    api_key = '07caae28dab3a3943e0d33a58d4a88fc'
+    base_url = 'https://api.themoviedb.org/3'
+
+    if search_query:
+
+        endpoint = '/search/movie'
+        params = {
+            'api_key': api_key,
+            'query': search_query,
+            'page': page_number
+        }
+
+    else:
+        endpoint = '/discover/movie'
+        params = {
+            'api_key': api_key,
+            'sort_by': 'popularity.desc',
+            'page': page_number
+        }
+    response = requests.get(f'{base_url}{endpoint}', params=params)
+    data = response.json()
+
+    movies = data.get('results', [])
+    total_pages = data.get('total_pages', 1)
+
+    if int(page_number) > 1:
+        prev_page = int(page_number) - 1
+    else:
+        prev_page = None
+    if int(page_number) < int(total_pages):
+        next_page = int(page_number) + 1
+    else:
+        next_page = None
+
+    return render(request, 'movies/index.html',
+                  {'movies': movies, 'search_query': search_query, 'total_pages': total_pages,
+                   'next_page': next_page, 'prev_page': prev_page, 'page_number': page_number
+                   })
+
+
+def movie_detail(request, movie_id):
+    api_key = '07caae28dab3a3943e0d33a58d4a88fc'
+    base_url = 'https://api.themoviedb.org/3'
+    endpoint = f"/movie/{movie_id}"
+    params = {'api_key': api_key}
+    response = requests.get(f'{base_url}{endpoint}', params=params)
+    movie = response.json()
+
+    return render(request, 'movies/detail.html', {'movie': movie})
