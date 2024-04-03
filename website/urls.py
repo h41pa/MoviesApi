391473fec8f5041f6349e4ab56ba60ccaa8d5924
h41pa <madalin_chelu@yahoo.com>
2024-04-03 20:20:39 +0300

    push

--- website/urls.py
+++ website/urls.py
@@ -24,7 +24,3 @@ urlpatterns = [
     path('', include('movies.urls'), name='movies')
 ]
 
-# from django.conf import settings
-# from django.conf.urls.static import static
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
