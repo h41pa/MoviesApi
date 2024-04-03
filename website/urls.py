fe7c2732fe84aefadd299cba2ea00dbc7db97511
h41pa <madalin_chelu@yahoo.com>
2024-04-03 12:52:52 +0300

    push

--- website/urls.py
+++ website/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,4 @@ urlpatterns = [
 
 # from django.conf import settings
 # from django.conf.urls.static import static
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
