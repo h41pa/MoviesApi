4439cc6f786c89a38a13204c2ba480e32c3d1b10
h41pa <madalin_chelu@yahoo.com>
2024-04-19 22:28:33 +0300

    push

--- website/settings.py
+++ website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d7#$smap4$9wfntbda8uabp+57v6gm*=1t5i#-=q-&9#n$&56m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
